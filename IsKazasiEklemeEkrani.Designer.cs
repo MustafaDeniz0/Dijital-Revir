9e0fce562a97c374207b77c480c981556a913e21
mustafadeniz0 <mustafa.deniz@ceng.deu.edu.tr>
2021-06-09 16:30:53 +0300

    Versiyon 1 (Sql Sorguları)

--- IsKazasiEklemeEkrani.Designer.cs
+++ IsKazasiEklemeEkrani.Designer.cs
@@ -38,11 +38,12 @@ namespace Dijital_Revir
             this.tbx_saat = new System.Windows.Forms.TextBox();
             this.dtp_kazaZamani = new System.Windows.Forms.DateTimePicker();
             this.tbx_calismaSuresi = new System.Windows.Forms.TextBox();
-            this.txb_vardiyaAmiri = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btn_Ekle = new System.Windows.Forms.Button();
+            this.cmb_VardiyaAmiri = new System.Windows.Forms.ComboBox();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -52,32 +53,30 @@ namespace Dijital_Revir
             this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(258, 673);
+            this.panel1.Size = new System.Drawing.Size(258, 538);
             this.panel1.TabIndex = 6;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel2.Controls.Add(this.btn_Ekle);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.btn_aktar);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel2.Location = new System.Drawing.Point(1004, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(258, 673);
+            this.panel2.Size = new System.Drawing.Size(258, 538);
             this.panel2.TabIndex = 5;
             // 
             // button1
             // 
             this.button1.AutoEllipsis = true;
             this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(39, 497);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(39, 398);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 65);
+            this.button1.Size = new System.Drawing.Size(177, 52);
             this.button1.TabIndex = 7;
             this.button1.Text = "Sevk Et";
             this.button1.UseVisualStyleBackColor = false;
@@ -87,11 +86,10 @@ namespace Dijital_Revir
             // 
             this.btn_aktar.AutoEllipsis = true;
             this.btn_aktar.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.btn_aktar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btn_aktar.Location = new System.Drawing.Point(39, 580);
-            this.btn_aktar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_aktar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn_aktar.Location = new System.Drawing.Point(39, 464);
             this.btn_aktar.Name = "btn_aktar";
-            this.btn_aktar.Size = new System.Drawing.Size(177, 65);
+            this.btn_aktar.Size = new System.Drawing.Size(177, 52);
             this.btn_aktar.TabIndex = 6;
             this.btn_aktar.Text = "Excel\'e Aktar";
             this.btn_aktar.UseVisualStyleBackColor = false;
@@ -99,73 +97,69 @@ namespace Dijital_Revir
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.cmb_VardiyaAmiri);
             this.panel3.Controls.Add(this.tbx_kazaAnlatimi);
             this.panel3.Controls.Add(this.tbx_saat);
             this.panel3.Controls.Add(this.dtp_kazaZamani);
             this.panel3.Controls.Add(this.tbx_calismaSuresi);
-            this.panel3.Controls.Add(this.txb_vardiyaAmiri);
             this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.label1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(258, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(746, 673);
+            this.panel3.Size = new System.Drawing.Size(746, 538);
             this.panel3.TabIndex = 7;
             // 
             // tbx_kazaAnlatimi
             // 
             this.tbx_kazaAnlatimi.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.tbx_kazaAnlatimi.Location = new System.Drawing.Point(266, 350);
+            this.tbx_kazaAnlatimi.Location = new System.Drawing.Point(266, 280);
+            this.tbx_kazaAnlatimi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbx_kazaAnlatimi.Multiline = true;
             this.tbx_kazaAnlatimi.Name = "tbx_kazaAnlatimi";
-            this.tbx_kazaAnlatimi.Size = new System.Drawing.Size(408, 212);
+            this.tbx_kazaAnlatimi.Size = new System.Drawing.Size(408, 170);
             this.tbx_kazaAnlatimi.TabIndex = 24;
             // 
             // tbx_saat
             // 
             this.tbx_saat.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.tbx_saat.Location = new System.Drawing.Point(516, 253);
+            this.tbx_saat.Location = new System.Drawing.Point(516, 202);
+            this.tbx_saat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbx_saat.Multiline = true;
             this.tbx_saat.Name = "tbx_saat";
-            this.tbx_saat.Size = new System.Drawing.Size(59, 28);
+            this.tbx_saat.Size = new System.Drawing.Size(59, 23);
             this.tbx_saat.TabIndex = 23;
             this.tbx_saat.Text = "00 : 00";
             // 
             // dtp_kazaZamani
             // 
             this.dtp_kazaZamani.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.dtp_kazaZamani.Location = new System.Drawing.Point(266, 254);
+            this.dtp_kazaZamani.Location = new System.Drawing.Point(266, 203);
+            this.dtp_kazaZamani.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtp_kazaZamani.Name = "dtp_kazaZamani";
-            this.dtp_kazaZamani.Size = new System.Drawing.Size(195, 27);
+            this.dtp_kazaZamani.Size = new System.Drawing.Size(195, 22);
             this.dtp_kazaZamani.TabIndex = 22;
             // 
             // tbx_calismaSuresi
             // 
             this.tbx_calismaSuresi.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.tbx_calismaSuresi.Location = new System.Drawing.Point(266, 188);
+            this.tbx_calismaSuresi.Location = new System.Drawing.Point(266, 150);
+            this.tbx_calismaSuresi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbx_calismaSuresi.Multiline = true;
             this.tbx_calismaSuresi.Name = "tbx_calismaSuresi";
-            this.tbx_calismaSuresi.Size = new System.Drawing.Size(408, 34);
+            this.tbx_calismaSuresi.Size = new System.Drawing.Size(408, 28);
             this.tbx_calismaSuresi.TabIndex = 21;
-            // 
-            // txb_vardiyaAmiri
-            // 
-            this.txb_vardiyaAmiri.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txb_vardiyaAmiri.Location = new System.Drawing.Point(266, 111);
-            this.txb_vardiyaAmiri.Multiline = true;
-            this.txb_vardiyaAmiri.Name = "txb_vardiyaAmiri";
-            this.txb_vardiyaAmiri.Size = new System.Drawing.Size(408, 34);
-            this.txb_vardiyaAmiri.TabIndex = 20;
             // 
             // label4
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.LightSalmon;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(72, 350);
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(72, 280);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(163, 28);
             this.label4.TabIndex = 19;
@@ -176,8 +170,8 @@ namespace Dijital_Revir
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.LightSalmon;
-            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(72, 254);
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(72, 203);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(126, 28);
             this.label3.TabIndex = 18;
@@ -188,8 +182,8 @@ namespace Dijital_Revir
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.LightSalmon;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(72, 188);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(72, 150);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(142, 28);
             this.label2.TabIndex = 17;
@@ -200,27 +194,52 @@ namespace Dijital_Revir
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.LightSalmon;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(72, 111);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(72, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(132, 28);
             this.label1.TabIndex = 16;
             this.label1.Text = "Vardiya Amiri";
             // 
+            // btn_Ekle
+            // 
+            this.btn_Ekle.AutoEllipsis = true;
+            this.btn_Ekle.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_Ekle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btn_Ekle.Location = new System.Drawing.Point(39, 327);
+            this.btn_Ekle.Name = "btn_Ekle";
+            this.btn_Ekle.Size = new System.Drawing.Size(177, 52);
+            this.btn_Ekle.TabIndex = 8;
+            this.btn_Ekle.Text = "Ekle";
+            this.btn_Ekle.UseVisualStyleBackColor = false;
+            this.btn_Ekle.Click += new System.EventHandler(this.btn_Ekle_Click);
+            // 
+            // cmb_VardiyaAmiri
+            // 
+            this.cmb_VardiyaAmiri.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cmb_VardiyaAmiri.FormattingEnabled = true;
+            this.cmb_VardiyaAmiri.Location = new System.Drawing.Point(266, 93);
+            this.cmb_VardiyaAmiri.Name = "cmb_VardiyaAmiri";
+            this.cmb_VardiyaAmiri.Size = new System.Drawing.Size(408, 24);
+            this.cmb_VardiyaAmiri.TabIndex = 25;
+            this.cmb_VardiyaAmiri.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // ekran_IsKazasıEkleme
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSalmon;
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(1262, 538);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ekran_IsKazasıEkleme";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "İş Kazası Ekleme";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ekran_IsKazasıEkleme_Load);
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -238,11 +257,12 @@ namespace Dijital_Revir
         private System.Windows.Forms.TextBox tbx_saat;
         private System.Windows.Forms.DateTimePicker dtp_kazaZamani;
         private System.Windows.Forms.TextBox tbx_calismaSuresi;
-        private System.Windows.Forms.TextBox txb_vardiyaAmiri;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Ekle;
+        private System.Windows.Forms.ComboBox cmb_VardiyaAmiri;
     }
 }
--- IsKazasiEklemeEkrani.cs
+++ IsKazasiEklemeEkrani.cs
@@ -42,5 +42,39 @@ namespace Dijital_Revir
             Form form = new ekran_SevkEtme();
             form.ShowDialog();
         }
+
+        private void btn_Ekle_Click(object sender, EventArgs e)
+        {
+            DateTime date = dtp_kazaZamani.Value;
+
+
+
+            //String sqlText = "Insert Into IsKazası(amirId, kazaZamanı, kazaninAnlatımı, personelId) " +
+            //"Values('" +  + "','" + tbx_sikayet.Text + "','" + tbx_tani.Text + "','" + tbx_tedavi.Text + "','" + tbx_doktor.Text + "');";
+            //SqlOps.SqlExecute(sqlText, null, SqlOps.GetSqlConnection());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ekran_IsKazasıEkleme_Load(object sender, EventArgs e)
+        {
+            cmb_VardiyaAmiri.Items.Clear();
+            String sqlText = "Select Distinct OzlukBilgileri.ad,OzlukBilgileri.soyAd From Personel P1 , Personel P2 left Join OzlukBilgileri on OzlukBilgileri.id = P2.ozlukId where P1.amir = P2.id";
+            DataTable dt = SqlOps.CreateDataTableBySqlQuery(sqlText);
+            
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                String obj = dr["ad"] + " " + dr["soyAd"];
+                cmb_VardiyaAmiri.Items.Add(new System.Collections.DictionaryEntry(obj, dr["soyAd"].ToString()));
+            }
+
+            cmb_VardiyaAmiri.DisplayMember = "Key";
+            cmb_VardiyaAmiri.ValueMember = "Value";
+            cmb_VardiyaAmiri.DataSource = cmb_VardiyaAmiri.Items;
+        }
     }
 }
